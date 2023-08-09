8a8e8c6aaef6ccb6d61c1011b8b29dc87b197d45
STones_ <61647649@qq.com>
2023-08-09 10:52:23 +0800

    add

--- app/grpcx/proto/test/test_grpc.pb.go
+++ app/grpcx/proto/test/test_grpc.pb.go
@@ -22,7 +22,8 @@ const _ = grpc.SupportPackageIsVersion7
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type TestInfoClient interface {
-	AddSomething(ctx context.Context, in *Test, opts ...grpc.CallOption) (*TestID, error)
+	ServerGetTestID(ctx context.Context, in *Test, opts ...grpc.CallOption) (*TestID, error)
+	ServerStreamEcho(ctx context.Context, opts ...grpc.CallOption) (TestInfo_ServerStreamEchoClient, error)
 }
 
 type testInfoClient struct {
@@ -33,20 +34,52 @@ func NewTestInfoClient(cc grpc.ClientConnInterface) TestInfoClient {
 	return &testInfoClient{cc}
 }
 
-func (c *testInfoClient) AddSomething(ctx context.Context, in *Test, opts ...grpc.CallOption) (*TestID, error) {
+func (c *testInfoClient) ServerGetTestID(ctx context.Context, in *Test, opts ...grpc.CallOption) (*TestID, error) {
 	out := new(TestID)
-	err := c.cc.Invoke(ctx, "/proto.TestInfo/AddSomething", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/proto.TestInfo/ServerGetTestID", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
+func (c *testInfoClient) ServerStreamEcho(ctx context.Context, opts ...grpc.CallOption) (TestInfo_ServerStreamEchoClient, error) {
+	stream, err := c.cc.NewStream(ctx, &TestInfo_ServiceDesc.Streams[0], "/proto.TestInfo/ServerStreamEcho", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &testInfoServerStreamEchoClient{stream}
+	return x, nil
+}
+
+type TestInfo_ServerStreamEchoClient interface {
+	Send(*TestRequest) error
+	Recv() (*TestResponse, error)
+	grpc.ClientStream
+}
+
+type testInfoServerStreamEchoClient struct {
+	grpc.ClientStream
+}
+
+func (x *testInfoServerStreamEchoClient) Send(m *TestRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *testInfoServerStreamEchoClient) Recv() (*TestResponse, error) {
+	m := new(TestResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // TestInfoServer is the server API for TestInfo service.
 // All implementations must embed UnimplementedTestInfoServer
 // for forward compatibility
 type TestInfoServer interface {
-	AddSomething(context.Context, *Test) (*TestID, error)
+	ServerGetTestID(context.Context, *Test) (*TestID, error)
+	ServerStreamEcho(TestInfo_ServerStreamEchoServer) error
 	mustEmbedUnimplementedTestInfoServer()
 }
 
@@ -54,8 +87,11 @@ type TestInfoServer interface {
 type UnimplementedTestInfoServer struct {
 }
 
-func (UnimplementedTestInfoServer) AddSomething(context.Context, *Test) (*TestID, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method AddSomething not implemented")
+func (UnimplementedTestInfoServer) ServerGetTestID(context.Context, *Test) (*TestID, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ServerGetTestID not implemented")
+}
+func (UnimplementedTestInfoServer) ServerStreamEcho(TestInfo_ServerStreamEchoServer) error {
+	return status.Errorf(codes.Unimplemented, "method ServerStreamEcho not implemented")
 }
 func (UnimplementedTestInfoServer) mustEmbedUnimplementedTestInfoServer() {}
 
@@ -70,22 +106,48 @@ func RegisterTestInfoServer(s grpc.ServiceRegistrar, srv TestInfoServer) {
 	s.RegisterService(&TestInfo_ServiceDesc, srv)
 }
 
-func _TestInfo_AddSomething_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _TestInfo_ServerGetTestID_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Test)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(TestInfoServer).AddSomething(ctx, in)
+		return srv.(TestInfoServer).ServerGetTestID(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/proto.TestInfo/AddSomething",
+		FullMethod: "/proto.TestInfo/ServerGetTestID",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TestInfoServer).AddSomething(ctx, req.(*Test))
+		return srv.(TestInfoServer).ServerGetTestID(ctx, req.(*Test))
 	}
 	return interceptor(ctx, in, info, handler)
+}
+
+func _TestInfo_ServerStreamEcho_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(TestInfoServer).ServerStreamEcho(&testInfoServerStreamEchoServer{stream})
+}
+
+type TestInfo_ServerStreamEchoServer interface {
+	Send(*TestResponse) error
+	Recv() (*TestRequest, error)
+	grpc.ServerStream
+}
+
+type testInfoServerStreamEchoServer struct {
+	grpc.ServerStream
+}
+
+func (x *testInfoServerStreamEchoServer) Send(m *TestResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *testInfoServerStreamEchoServer) Recv() (*TestRequest, error) {
+	m := new(TestRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 // TestInfo_ServiceDesc is the grpc.ServiceDesc for TestInfo service.
@@ -96,10 +158,17 @@ var TestInfo_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*TestInfoServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "AddSomething",
-			Handler:    _TestInfo_AddSomething_Handler,
+			MethodName: "ServerGetTestID",
+			Handler:    _TestInfo_ServerGetTestID_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "ServerStreamEcho",
+			Handler:       _TestInfo_ServerStreamEcho_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
+		},
+	},
 	Metadata: "test/test.proto",
 }
