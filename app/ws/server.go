86bdbec08084add32a842e8b2ea1c1e0f5d4fb97
STones_ <61647649@qq.com>
2023-08-11 09:59:09 +0800

    add

--- app/ws/server.go
+++ app/ws/server.go
@@ -1,6 +1,10 @@
 package ws
 
-import "context"
+import (
+	"context"
+	"k3gin/app/config"
+	"k3gin/app/logger"
+)
 
 type options struct {
 	configFile string
@@ -14,5 +18,23 @@ func WithConfigFile(configFile string) func(*options) {
 
 // Run WebSocket Server
 func Run(ctx context.Context, opts ...func(*options)) error {
+
+	var o options
+
+	for _, opt := range opts {
+		opt(&o)
+	}
+
+	config.MustLoad(o.configFile)
+	config.PrintWithJSON()
+
+	cleanFunc, err := logger.InitLogger()
+	if err != nil {
+		return err
+	}
+
+	// 处理优雅退出
+
+	cleanFunc()
 	return nil
 }
