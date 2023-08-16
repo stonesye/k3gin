b73ccf5d2101ea9016e90a19c4b23fab11b76b62
STones_ <61647649@qq.com>
2023-08-16 18:19:52 +0800

    add

--- app/ws/ws_api/interface.go
+++ app/ws/ws_api/interface.go
@@ -5,6 +5,7 @@ import (
 	"k3gin/app/ws/ws_context"
 )
 
+// ApiInterface 所有的API都需要实现它
 type ApiInterface interface {
 	write(*ws_context.WSContext, *websocket.Conn)
 	read(*ws_context.WSContext, *websocket.Conn)
