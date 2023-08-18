7880b2f5a891a7ce8370885c03cee262d849d842
STones_ <61647649@qq.com>
2023-08-18 16:52:35 +0800

    add

--- app/ws/ws_api/test_api.go
+++ app/ws/ws_api/test_api.go
@@ -5,6 +5,7 @@ import (
 	"k3gin/app/cache/redisx"
 	"k3gin/app/gormx"
 	"k3gin/app/httpx"
+	"k3gin/app/logger"
 	"k3gin/app/ws/ws_context"
 	"k3gin/app/ws/ws_router"
 )
@@ -19,13 +20,30 @@ type Test struct {
 	Redis      *redisx.Store
 }
 
-var TestApiSet = wire.NewSet(wire.Struct(new(Test), "*"), wire.Bind(new(WSApi), new(*Test)))
+var TestApiSet = wire.NewSet(wire.Struct(new(Test), "*"))
 
 func (t *Test) TestApi(ctx *ws_context.WSContext) {
 	ws, cleanFunc, err := ws_router.NewWebsocketConnect(ctx)
 	if err != nil {
 		return
 	}
+
 	defer cleanFunc()
+
+	var p = make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(p)
+		if err != nil {
+			logger.WithFieldsFromWSContext(ctx).Errorf("Read message err[%v] : %v", n, err)
+			break
+		}
+
+		n, err = ws.Write(p)
+		if err != nil {
+			logger.WithFieldsFromWSContext(ctx).Errorf("Recv message err[%v] : %v", n, err)
+			break
+		}
+	}
 
 }
