4f17f289f75acadeda1e0fb21b9533f778f2f830
STones_ <61647649@qq.com>
2023-08-18 11:57:30 +0800

    add

--- app/ws/ws_api/test_api.go
+++ app/ws/ws_api/test_api.go
@@ -2,13 +2,11 @@ package ws_api
 
 import (
 	"github.com/google/wire"
-	"github.com/gorilla/websocket"
-	"io"
 	"k3gin/app/cache/redisx"
 	"k3gin/app/gormx"
 	"k3gin/app/httpx"
-	"k3gin/app/logger"
 	"k3gin/app/ws/ws_context"
+	"k3gin/app/ws/ws_router"
 )
 
 /**
@@ -24,42 +22,10 @@ type Test struct {
 var TestApiSet = wire.NewSet(wire.Struct(new(Test), "*"), wire.Bind(new(WSApi), new(*Test)))
 
 func (t *Test) TestApi(ctx *ws_context.WSContext) {
-	conn, err := ctx.Upgrader.Upgrade(ctx.GinCtx.Writer, ctx.GinCtx.Request, nil)
+	ws, cleanFunc, err := ws_router.NewWebsocketConnect(ctx)
 	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			logger.WithFieldsFromWSContext(ctx).Errorf("websocket error: %v", err)
-		}
 		return
 	}
+	defer cleanFunc()
 
-	defer conn.Close()
-
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			logger.WithFieldsFromWSContext(ctx).Errorf("Failed to read message from ws : %v", err)
-		}
-
-		switch messageType {
-		case websocket.TextMessage:
-			err := conn.WriteMessage(messageType, p)
-			break
-
-		case websocket.BinaryMessage:
-			err := conn.WriteMessage(messageType, p)
-			break
-		}
-	}
-}
-
-func (t *Test) Read(p []byte) (n int, err error) {
-	panic("implement me")
-}
-
-func (t *Test) Write(p []byte) (n int, err error) {
-	panic("implement me")
-}
-
-func (t *Test) Copy(writer io.Writer, reader io.Reader) (int64, error) {
-	panic("implement me")
 }
--- app/ws/ws_router/router.go
+++ app/ws/ws_router/router.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
 	"github.com/gorilla/websocket"
+	"io"
+	"k3gin/app/logger"
 	"k3gin/app/ws/ws_api"
 	"k3gin/app/ws/ws_context"
 	"net/http"
@@ -50,4 +52,79 @@ func WithWSContext(handler func(*ws_context.WSContext)) func(*gin.Context) {
 		}
 		handler(&ctx)
 	}
+}
+
+type IWSProxy interface {
+	Read(p []byte) (n int, err error)
+	Write(p []byte) (n int, err error)
+	Copy(io.Writer, io.Reader) (int64, error)
+}
+
+type WSProxy struct {
+	ctx    *ws_context.WSContext
+	ws     *websocket.Conn
+	wsType int
+}
+
+func NewWebsocketConnect(ctx *ws_context.WSContext) (*WSProxy, func(), error) {
+	ws, err := ctx.Upgrader.Upgrade(ctx.GinCtx.Writer, ctx.GinCtx.Request, nil)
+	if err != nil {
+		if _, ok := err.(websocket.HandshakeError); ok {
+			logger.WithFieldsFromWSContext(ctx).Errorf("Fetch websocket conn error : %v", err)
+		}
+		return nil, func() {}, err
+	}
+
+	return &WSProxy{ws: ws, ctx: ctx}, func() {
+		ws.Close()
+	}, nil
+}
+
+func (proxy *WSProxy) Read(p []byte) (n int, err error) {
+	proxy.wsType, p, err = proxy.ws.ReadMessage()
+	if err != nil {
+		if err != io.EOF {
+			logger.WithFieldsFromWSContext(proxy.ctx).Errorf("Read message error: %v", err)
+		}
+		return 0, err
+	}
+	return len(p), err
+}
+
+func (proxy *WSProxy) Write(p []byte) (n int, err error) {
+	err = proxy.ws.WriteMessage(proxy.wsType, p)
+	if err != nil {
+		if err != io.EOF {
+			logger.WithFieldsFromWSContext(proxy.ctx).Errorf("Writer message error: %v", err)
+		}
+		return 0, err
+	}
+	return len(p), err
+}
+
+func (proxy *WSProxy) Copy(writer io.Writer, reader io.Reader) (int64, error) {
+	buf := make([]byte, 4096)
+
+	var written int64 = 0
+
+	for {
+		n, err := reader.Read(buf)
+		if err != nil && err != io.EOF {
+			return 0, err
+		}
+		if n == 0 {
+			return written, nil
+		}
+
+		written += int64(n)
+		m, err := writer.Write(buf[:n])
+		if err != nil {
+			return written, err
+		}
+
+		if m > n {
+			return written, io.ErrShortWrite
+		}
+	}
+
 }
