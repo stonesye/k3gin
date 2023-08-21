a93d7b384f0237df56976cca339fd39dbbde20e0
STones_ <61647649@qq.com>
2023-08-21 19:47:03 +0800

    add

--- app/ws/ws_api/test_api.go
+++ app/ws/ws_api/test_api.go
@@ -5,8 +5,11 @@ import (
 	"k3gin/app/cache/redisx"
 	"k3gin/app/gormx"
 	"k3gin/app/httpx"
+	"k3gin/app/logger"
+	"k3gin/app/util/trace"
 	"k3gin/app/ws/ws_context"
 	"k3gin/app/ws/ws_proxy"
+	"time"
 )
 
 /**
@@ -28,4 +31,33 @@ func (t *Test) TestApi(ctx *ws_context.WSContext) {
 	}
 	defer cleanFunc()
 
+	var hbTime = time.Second * time.Duration(10)
+
+	hbTicker := time.NewTicker(hbTime)
+	defer hbTicker.Stop()
+
+	go func() {
+		for {
+			p := make([]byte, 1024)
+			if _, err := ws.Read(p); err != nil {
+				logger.WithFieldsFromContext(ctx).Errorf("Read client message err : %v", err)
+				return
+			}
+			// 回写数据给客户端
+			res := trace.NewTraceID() + "-" + string(p)
+			if _, err := ws.Write([]byte(res)); err != nil {
+				logger.WithFieldsFromContext(ctx).Errorf("write message err : %v", err)
+				return
+			}
+			hbTicker.Reset(hbTime)
+		}
+	}()
+
+	for {
+		select {
+		case <-hbTicker.C:
+			ws.Write([]byte("链接长时间没有信息，已关闭."))
+			return
+		}
+	}
 }
